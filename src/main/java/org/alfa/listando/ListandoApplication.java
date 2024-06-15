51b3c02fd6f352b56e121bdb3311bfaaec27e747
Marco Sousa <marcopaiva2004@gmail.com>
2024-06-15 18:28:59 +0200

    Initial commit

---
+++ src/main/java/org/alfa/listando/ListandoApplication.java
@@ -0,0 +1,13 @@
+package org.alfa.listando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alfa/listando/ListandoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alfa.listando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
